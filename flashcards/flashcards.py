348b79f83f7de9c6dfbbb9ef36d09e6218016c82
Luis Naranjo <luisnaranjo733@hotmail.com>
2012-05-10 01:47:04 -0700

    Removed the huge docstring

--- flashcards/flashcards.py
+++ flashcards/flashcards.py
@@ -1,30 +1,3 @@
-"""class User(Base):
-    __tablename__ = 'user'
-
-    id = Column(Integer, primary_key=True)
-    date_created = Column(DateTime)
-    username = Column(String)
-    password = Column(String)
-    decks = relationship('Deck')
-
-
-class Deck(Base):
-    __tablename__ = 'deck'
-    id = Column(Integer, primary_key=True)
-    date_created = Column(DateTime)
-    name = Column(String)
-    flashcards = relationship("Flashcard")  # List
-    user_id = Column(Integer, ForeignKey('user.id'))  # If no relationship - null
-
-
-class Flashcard(Base):
-    __tablename__ = 'flashcard'
-    id = Column(Integer, primary_key=True)
-    date_created = Column(DateTime)
-    question = Column(String)
-    answers = Column(PickleType)
-    deck_id = Column(Integer, ForeignKey('deck.id'))"""
-
 from models import session, DEBUG, User, Deck, Flashcard, CardBox
 import logging
 
